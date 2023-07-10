119ef47c4d2fe8a13a78065bb25549f1333d1ca5
Qin Guan <qinguan20040914@gmail.com>
2023-07-11 07:46:19 +0800

    debug

--- middleware/guest.ts
+++ middleware/guest.ts
@@ -2,6 +2,7 @@ export default defineNuxtRouteMiddleware(async () => {
   const { $client } = useNuxtApp()
 
   const { data } = await $client.me.get.useQuery()
+  console.log(data.value)
   if (data.value?.id)
     return '/dashboard'
 })
