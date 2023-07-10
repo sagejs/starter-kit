3d2ef8383cbe144955631e3cded21b44a91514f3
Qin Guan <qinguan20040914@gmail.com>
2023-07-11 07:42:00 +0800

    fix: prerendering

--- middleware/guest.ts
+++ middleware/guest.ts
@@ -2,6 +2,6 @@ export default defineNuxtRouteMiddleware(async () => {
   const { $client } = useNuxtApp()
 
   const { data } = await $client.me.get.useQuery()
-  if (data.value.id)
+  if (data.value?.id)
     return '/dashboard'
 })
